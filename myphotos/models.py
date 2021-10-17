c3447e055464551e71710cc42f332ec44b63b6ee
Peter Gakure <petergakure97@gmail.com>
2021-10-17 16:45:35 +0300

    Create Image model

--- myphotos/models.py
+++ myphotos/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.db.models.manager import Manager
 
 # Create your models here.
+class Image(models.Model):
+    image = models.ImageField()
+    name = models.CharField(max_length=30)
+    imagecaption = models.TextField()
+    profile = models.ForeignKey(pk= id)
+    likes = models.CharField(max_length=30)
+    comments = models.CharField(max_length=150)
+
+class Profile(models.Model):
+    profilephoto = models.ImageField()
+    bio = models.CharField(max_length=200)
+        
