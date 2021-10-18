03895ac5cb3c31f4639deddc56de29cc7451ba0f
Peter Gakure <petergakure97@gmail.com>
2021-10-18 09:19:47 +0300

    Create new forms model

---
+++ myphotos/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+from .models import Profile, Comment
+
+
+class ProfileForm(forms.ModelForm):
+	model = Profile
+	username = forms.CharField(label='Username',max_length = 30)
+	
+	bio = forms.CharField(label='Image Caption',max_length=500)
+	profile_pic = forms.ImageField(label = 'Image Field')
+
+
+class UploadForm(forms.ModelForm):
+	class Meta:
+		model = Profile
+		
+		exclude = ['user']
+
+class CommentForm(forms.ModelForm):
+	class Meta:
+		model = Comment
+		
+		exclude = ['user','pic',]
