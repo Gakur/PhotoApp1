e6b6b7467a68f056a85d200ae81418f554a6bbff
Peter Gakure <petergakure97@gmail.com>
2021-10-18 22:50:43 +0300

    Update file

--- photos/settings.py
+++ photos/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'myphotos',
+    'bootstrap5',
+    'tinymce',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
