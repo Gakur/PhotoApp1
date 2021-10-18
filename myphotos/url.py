acb62e3a5a5b8be583a24804c55c63940377b21e
Peter Gakure <petergakure97@gmail.com>
2021-10-18 14:45:09 +0300

    Add new path

--- myphotos/url.py
+++ myphotos/url.py
@@ -3,5 +3,10 @@ from . import views
 
 urlpatterns=[
     path('',views.index, name = 'index'),
-    path('')
+    path('post/<id>', views.comment, name = 'comment'),
+    path('user_profile/<username>/', views.user_profile, name='user_profile'),
+    path('like', views.like, name='like_post'),
+    path('search/', views.search_results, name='search'),
+    path('upload/profile', views.upload_profile, name='upload_profile'),
+    path('follow/<to_follow>', views.follow, name='follow'),
 ]
