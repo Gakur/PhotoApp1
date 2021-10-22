0560ed2b2c6cce94c835265203a3af55be010a4b
Peter Gakure <petergakure97@gmail.com>
2021-10-22 10:56:29 +0300

    import the settings from django.config and update new path

---
+++ myphotos/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns=[
+    path('',views.index, name = 'index'),
+    path('comment/<id>', views.comment, name = 'comment'),
+    path('user_profile/<username>/', views.user_profile, name='user_profile'),
+    path('like', views.like, name='like_post'),
+    path('search/', views.search_results, name='search'),
+    path('upload_profile/', views.upload_profile, name='upload_profile'),
+    path('follow/<to_follow>', views.follow, name='follow'),
+]
+
+if settings.DEBUG:
+	urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT) 
